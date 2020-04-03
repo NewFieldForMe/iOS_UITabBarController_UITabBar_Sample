03ff8aa6a97173f2050718d09e0fa42e4e1765a3
RyoYamada <newfield1001@gmail.com>
2020-04-03 17:52:15 +0900

    initial commit

---
+++ TryOutProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TryOutProject
+//
+//  Created by 山田良 on 2020/04/03.
+//  Copyright © 2020 yamadaryo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
