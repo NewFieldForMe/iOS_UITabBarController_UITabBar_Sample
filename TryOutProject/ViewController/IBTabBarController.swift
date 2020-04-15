3eec657835b4a65d58d555c73317eeffd3a3a211
RyoYamada <newfield1001@gmail.com>
2020-04-16 00:39:00 +0900

    Add TabBarController to storyboard

---
+++ TryOutProject/ViewController/IBTabBarController.swift
@@ -0,0 +1,12 @@
+//
+//  IBTabBarController.swift
+//  TryOutProject
+//
+//  Created by 山田良 on 2020/04/15.
+//  Copyright © 2020 yamadaryo. All rights reserved.
+//
+
+import UIKit
+
+class IBTabBarController: UITabBarController {
+}
